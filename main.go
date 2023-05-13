07b1e8ecd1c1064aa338681ba031c3e9f59dbf94
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-13 05:00:32 -0700

    Add readme

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-
-	git "github.com/taylormonacelli/flashbiter/git"
+	"text/template"
+	"time"
 
 	"github.com/Pallinder/go-randomdata"
 	"github.com/atotto/clipboard"
 	"github.com/gdamore/tcell/v2"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/rivo/tview"
 	log "github.com/taylormonacelli/reactnut/cmd/logging"
 )
@@ -81,7 +83,7 @@ func allowUserToSelectItem(selectables []string) (string, error) {
 		ShowSecondaryText(false).
 		SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
 			selectedItem = selectables[index]
-			returnValue(selectedItem, &ClipboardDestination{})
+			// returnValue(selectedItem, &ClipboardDestination{})
 			// returnValue(selectedItem, &ConsoleDestination{})
 			// returnValue(selectedItem, &FileDestination{FilePath: "items.txt"})
 			app.Stop()
@@ -207,6 +209,115 @@ func selectPath(paths map[string]string) (string, error) {
 	return paths[item], nil
 }
 
+func GitInit(path string) error {
+	_, err := git.PlainInit(path, false)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func gitCommitReadme(dir string) error {
+	// Define the template string
+	const tmpl = `# {{.Title}}
+
+{{.Description}}
+
+## Installation
+
+{{.Installation}}
+
+## Usage
+
+{{.Usage}}
+`
+
+	// Define the data for the template
+	data := struct {
+		Title        string
+		Description  string
+		Installation string
+		Usage        string
+	}{
+		Title:        "My Project",
+		Description:  "This is a description of my project.",
+		Installation: "To install my project, run this command: go install myproject",
+		Usage:        "To use my project, run this command: myproject",
+	}
+
+	// Create a new directory and its parent directories
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	filename := "README.md"
+	fullPath := filepath.Join(dir, filename)
+
+	// Create the new file
+	file, err := os.Create(fullPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Parse the template
+	tpl, err := template.New("README").Parse(tmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	// Execute the template with the data and write the output to the file
+	err = tpl.Execute(file, data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Open the repository
+	repo, err := git.PlainOpen(dir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Create the new file
+	file, err = os.Create("README.md")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer file.Close()
+
+	// Add the new file to the repository
+	worktree, err := repo.Worktree()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	_, err = worktree.Add("README.md")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Commit the changes
+	_, err = worktree.Commit("Add README.md", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Your Name",
+			Email: "your.email@example.com",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return nil
+}
+
 func main() {
 	baseDir := getBaseDir()
 	myMap := generateUniquePaths(baseDir, 35)
@@ -214,6 +325,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	git.GitInit(selectedPath)
+	GitInit(selectedPath)
+	_ = gitCommitReadme(selectedPath)
 	fmt.Println(selectedPath)
 }
